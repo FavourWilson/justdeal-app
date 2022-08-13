bd3dde12ce8963db5eb143d466944d738f4465c6
Matthew Okiemute <Ego.okiemute@gmail.com>
2022-08-13 20:25:58 +0100

    build: made changes to the build

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,29 @@
-// import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/solid';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-// import Logo from '../public/logo.png';
+import Logo from '../public/jd-logo.png';
+import headerStyles from '../styles/Header.module.css';
 
 const Header = () => {
   return (
-    <header className="shadow-md px-4">
+    <header className="px-4 fixed top-0 right-0 left-0">
       <div className="xl:container xl:mx-auto flex flex-col h-20 py-6 items-center align-items-center sm:flex-row sm:justify-between">
-        <div className="flex flex-row align-items-center justify-center">
-          {/* <Image
+        <div className="flex flex-row items-center justify-center">
+          <Image
             src={Logo}
             alt="Main logo"
-            width={90}
+            width={200}
             height={40}
-            className="mr-5"
-          /> */}
-          <h1 className="logo font-bold mr-4 block text-base">JustdealNG</h1>
+            className="inline-block mr-10 "
+          />
+          {/* <h1 className="logo font-bold mr-4 block text-base">JustdealNG</h1> */}
+
+          <SearchIcon className="h-5 w-5 text-gray-500 hey" />
           <input
             type="text"
             placeholder="Search..."
-            className="p-2 bg-transparent border-solid border-gray-400 rounded-lg search-input"
+            className={headerStyles.searchBox}
           />
         </div>
         <div>
