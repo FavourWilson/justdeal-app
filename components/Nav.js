18914091f94aa4d8e824ef53461257a7b207bad7
brandmalzy <emmanuel.a.ego@gmail.com>
2022-08-22 23:58:20 +0100

    update: added hover to the login and register botton

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,25 @@
-import cat from "../data/cats";
-import NavItems from "./NavItems";
+import Link from 'next/link';
+import cat from '../data/cats';
 import headerStyles from '../styles/Header.module.css';
 
 const Nav = () => {
   return (
-        <nav className={headerStyles.nav}>
-          <div className="xl:container xl:mx-auto flex flex-col h-20 py-2 items-center align-items-center sm:flex-row sm:justify-between">
-            {cat.categories.map((categories) => (
-              <NavItems categories={categories} key={categories.category}>
-                {categories.category}
-              </NavItems>
-            ))}
+    <nav className={headerStyles.nav}>
+      <div className="xl:container xl:mx-auto flex flex-col h-12 py-2 items-center align-items-center sm:flex-row sm:justify-between">
+        {cat.categories.map((categories) => (
+          <div categories={categories} key={categories.category}>
+            <ul className="list-none">
+              <li>
+                <Link href={`/${categories.slug}`}>
+                  <a>{categories.category}</a>
+                </Link>
+              </li>
+            </ul>
           </div>
-        </nav>
+        ))}
+      </div>
+    </nav>
   );
-}
- 
+};
+
 export default Nav;
